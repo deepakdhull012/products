accbefcfad78cc66e9468b3e1bf2bcfdef550b01
Deepak Dhull <deepakdhull012@gmail.com>
2024-10-31 18:23:59 +0530

    Initial commit

---
+++ src/main/java/com/deepak/ecommerce/products/products/ProductImage.java
@@ -0,0 +1,11 @@
+package com.deepak.ecommerce.products.products;
+
+import lombok.Data;
+
+@Data
+public class ProductImage {
+
+    private String imageUrl;
+
+    private boolean isPrimary;
+}
---
+++ src/main/java/com/deepak/ecommerce/products/products/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.deepak.ecommerce.products.products;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
